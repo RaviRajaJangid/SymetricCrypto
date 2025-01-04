c756a1710007cfc4341c719a7b60df08c30759db
Ravi Raja <raviraja.itpro@gmail.com>
2025-01-04 15:33:24 +0530

    initial commit

---
+++ SymetricCrypto/SymetricCryptoTests/SymetricCryptoTests.swift
@@ -0,0 +1,17 @@
+//
+//  SymetricCryptoTests.swift
+//  SymetricCryptoTests
+//
+//  Created by Ravi Raja Jangid on 04/01/25.
+//
+
+import Testing
+@testable import SymetricCrypto
+
+struct SymetricCryptoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
